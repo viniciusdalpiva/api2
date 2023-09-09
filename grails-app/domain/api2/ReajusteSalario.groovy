338773460b7a22aae8cabae6aad30c0c2214c914
viniciusdalpiva <viniciusdalpivadasilva@gmail.com>
2023-09-09 18:41:21 -0300

    fix: Ajustes no Domain ReajusteSalario

--- grails-app/domain/api2/ReajusteSalario.groovy
+++ grails-app/domain/api2/ReajusteSalario.groovy
@@ -1,19 +1,17 @@
 package api2
 
 import com.fasterxml.jackson.annotation.JsonFormat
-import com.fasterxml.jackson.annotation.JsonManagedReference
 import grails.converters.JSON
-
-import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class ReajusteSalario {
-
     @JsonFormat(pattern = "dd/MM/yyyy")
     LocalDate dataReajuste
     BigDecimal valorSalario
     Funcionario funcionario
+
+    static belongsTo = [funcionario: Funcionario]
 
     static {
         JSON.registerObjectMarshaller(LocalDate) { LocalDate date ->
@@ -23,12 +21,11 @@ class ReajusteSalario {
 
     static constraints = {
         dataReajuste nullable: false
-        valorSalario nullable: false, scale: 2
+        valorSalario nullable: false, precision: 6, scale: 2
         funcionario nullable: false
     }
-
-//    static mapping = {
-//        id column: "reajuste_id"
-//        version false
-//    }
+    static mapping = {
+        id generator: "increment"
+        version false
+    }
 }
