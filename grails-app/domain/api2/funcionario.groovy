4bcd9f98fa79347bb8081174a8d2fa8a8dac9b38
viniciusdalpiva <viniciusdalpivadasilva@gmail.com>
2023-09-09 18:38:46 -0300

    fix: Ajustes no Domain Funcionario

--- grails-app/domain/api2/funcionario.groovy
+++ grails-app/domain/api2/funcionario.groovy
@@ -2,16 +2,16 @@ package api2
 
 class Funcionario {
     String nome
-    Cidade cidade
+
+    static belongsTo = [cidade: Cidade]
 
     static constraints = {
         nome nullable: false, maxSize: 50
         cidade nullable: false
     }
-    static belongsTo = [cidade: Cidade]
 
-//    static mapping = {
-//        id column: "funcionario_id"
-//        version false
-//    }
+    static mapping = {
+        id generator: "increment"
+        version false
+    }
 }
