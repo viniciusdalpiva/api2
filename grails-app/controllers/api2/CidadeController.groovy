690583dcf7013623cffb8368c3571cd6bf2234ac
viniciusdalpiva <viniciusdalpivadasilva@gmail.com>
2023-09-12 22:08:51 -0300

    fix: cidade controller ajustes mensagem de erro delete para api3

--- grails-app/controllers/api2/CidadeController.groovy
+++ grails-app/controllers/api2/CidadeController.groovy
@@ -1,14 +1,15 @@
 package api2
 
 import grails.converters.JSON
+import org.springframework.dao.DataIntegrityViolationException
 
 class CidadeController {
 
     def cidadeService
 
-    def index() {
+    def list() {
         if (request.method != "GET") {
-            response.setStatus(405) // Método não permitido
+            response.setStatus(405)
             render "Método não permitido para esta ação." as JSON
             return
         }
@@ -17,9 +18,9 @@ class CidadeController {
         render cidades as JSON
     }
 
-    def show(Long id) {
+    def get(Long id) {
         if (request.method != "GET") {
-            response.setStatus(405) // Método não permitido
+            response.setStatus(405)
             render "Método não permitido para esta ação." as JSON
             return
         }
@@ -49,7 +50,6 @@ class CidadeController {
             render "Método não permitido para esta ação." as JSON
             return
         }
-
         try {
             def cidadeData = request.JSON
             def cidade = cidadeService.atualizarCidade(id, cidadeData)
@@ -62,18 +62,25 @@ class CidadeController {
     def delete(Long id) {
         if (request.method != "DELETE") {
             response.setStatus(405) // Método não permitido
-            render "Método não permitido para esta ação." as JSON
+            render "Método não permitido para esta ação."
             return
         }
-
-        def cidade = cidadeService.excluirCidade(id)
-        render cidade as JSON
-    }
-    private renderError(String errorMessage) {
-        render(contentType: 'application/json') {
-            message "Bad Request"
-            error 400
-            data "Dados inseridos de maneira incorreta!"
+        try {
+            def cidade = Cidade.get(id)
+            if (!cidade) {
+                render(status: 404, text: "Cidade não encontrada.")
+                return
+            }
+            def funcionarios = Funcionario.findAllByCidade(cidade)
+            if (funcionarios) {
+                throw new DataIntegrityViolationException("Não é possível excluir a cidade, pois existem funcionários associados a ela. Exclua os funcionários primeiro.")
+            }
+            cidade.delete()
+            render(status: 200, text: "Cidade ${id} deletada com sucesso!")
+        } catch (DataIntegrityViolationException e) {
+            render(status: 409, text: "Não é possível excluir a cidade, pois existem funcionários associados a ela. Exclua os funcionários primeiro.")
+        } catch (Exception e) {
+            render(status: 500, text: e.message)
         }
     }
 }
